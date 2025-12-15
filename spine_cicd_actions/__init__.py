66c56899e95a9abf4f547af73bbdc5afa56823ae
aviv ron <rona@il.ibm.com>
2025-12-15 21:16:25 +0200

    switch to python package structure

---
+++ spine_cicd_actions/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2025, The Spine Docs organization and its contributors.
+
+"""Spine CI/CD Actions - Pre-commit hooks and GitHub Actions."""
+
+__version__ = "1.0.0"
